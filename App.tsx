c2b774651d52cdf48d613c60f112c133807596b0
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-03-06 12:32:45 +0100

    Create basic project structure

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FC } from 'react';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import HomeScreen from './src/screens/HomeScreen'
+import RoomScreen from './src/screens/RoomScreen'
+
+const Stack = createStackNavigator()
+
+const App:FC = () => {
+  return (  
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Room" component={RoomScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/RoomScreen.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RoomScreen:FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>RoomScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default RoomScreen;
